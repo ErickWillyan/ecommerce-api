405656ec759b9ea8347245837dd668d16eab39a4
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2024-03-27 10:14:24 -0300

    feature edit prettier

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
     credentials: true,
   });
   await app.listen(4000);
