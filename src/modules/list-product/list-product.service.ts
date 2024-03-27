6b30e4eed46746d69b934f9c11e5ea039dddae5c
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2024-03-27 13:44:26 -0300

    update listProduct

--- src/modules/list-product/list-product.service.ts
+++ src/modules/list-product/list-product.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/PrismaService';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/PrismaService";
 
 @Injectable()
 export class ListProductService {
   constructor(private prisma: PrismaService) {}
 
   async listProduct() {
-    return await this.prisma.product.findMany();
+    return await this.prisma.product.findMany({
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        description: true,
+        categoryId: true,
+        image: true,
+        category: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 }
